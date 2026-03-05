71591f2c4ac1cc76873557c4ec407a98ea1413f5
Kartikey-Gupta826 <kartikeygupta261204@gmail.com>
2026-03-05 19:46:54 +0530

    segregating code in functions

--- demo/src/main/java/game/Main.java
+++ demo/src/main/java/game/Main.java
@@ -4,8 +4,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the game!");
+
+    public static void Description() {
         String temp = GeminiChat.chat(
                 "Provide a good game description in 40 words. The game is about a player who is on a journey to defeat villains and gain points. The player can move around, attack or defend against villains, and participate in a lottery system to restore HP. Try to use vowels less but lines must make sense. just try to use the word with less vowels.");
 
@@ -57,6 +57,139 @@ public class Main {
         } else {
             System.out.println("Welcome to this adventure :)".trim());
         }
+
+        return;
+    }
+
+    public static void Movement(String InGameMovement, int x_Coordinate, int y_Coordinate, String StartingPoint,
+            Scanner scanner) {
+        // Movement System
+
+        if (x_Coordinate == 0 && y_Coordinate == 0) {
+            System.out.println("You are at the " + StartingPoint);
+        }
+
+        System.out.println("Current Position: (" + x_Coordinate + ", " + y_Coordinate + ")");
+        System.out.println(
+                "Where do you want to move? (Type 'up', 'down', 'left', or 'right')(max x and y are (5,5) and min x and y are (0,0))");
+        InGameMovement = scanner.nextLine();
+
+        switch (InGameMovement.toLowerCase()) {
+
+            case "down":
+                if (y_Coordinate > 0) {
+                    y_Coordinate = y_Coordinate - 1;
+                }
+                break;
+            case "left":
+                if (x_Coordinate > 0) {
+                    x_Coordinate = x_Coordinate - 1;
+                }
+                break;
+            case "right":
+                if (x_Coordinate < 5) {
+                    x_Coordinate = x_Coordinate + 1;
+                }
+                break;
+            case "up":
+                if (y_Coordinate < 5) {
+                    y_Coordinate = y_Coordinate + 1;
+                }
+                break;
+            default:
+                System.out.println("Invalid movement, try again.");
+        }
+    }
+
+    public static void Villain(int x_Coordinate, int y_Coordinate, int counter, String PLAYERNAME, Scanner scanner, int hp, int score, int villain_level, Random random) {
+
+        // attack or defend
+        int x_Coordinate_v = random.nextInt(0, 5);
+        int y_Coordinate_v = random.nextInt(0, 5);
+
+        villain_level = random.nextInt(1, 11);
+
+        if (counter % 3 == 0) {
+            x_Coordinate_v = x_Coordinate;
+            y_Coordinate_v = y_Coordinate;
+        }
+        if ((x_Coordinate == x_Coordinate_v && y_Coordinate == y_Coordinate_v)) {
+            System.out.println("You are " + PLAYERNAME + "\n" + "A level " + villain_level
+                    + " villain has appear. BE PREPARED!!!");
+            GeminiChat.chat("You are a game villain. Choose a good name. You just encountered a player named "
+                    + PLAYERNAME + ". Introduce yourself dramatically in 30 words.");
+            System.out.println();
+            System.out.print(PLAYERNAME + " : ");
+            String reply = scanner.nextLine();
+
+            GeminiChat.chat(
+                    "You reply to the player reply in a way that after which is just action left. The reply is : "
+                            + reply);
+            System.out.println();
+
+            System.out.println("Do you want to attack or defend? (Type 'attack' or 'defend')");
+            String action = scanner.nextLine();
+
+            if (action.equalsIgnoreCase("attack")) {
+                hp = hp - 10 * villain_level;
+                score = score + 5 * villain_level;
+                GeminiChat.chat(
+                        "Vilain went away but deal some damage. Appropriate dialogue for it as a closing scene in 2 sentences.");
+                System.out.println();
+
+            } else if (action.equalsIgnoreCase("defend")) {
+                hp = hp - 1 * villain_level;
+                GeminiChat.chat(
+                        "Villain does not went away and deal some damage. Appropriate dialogue for it as a closing scene in 2 sentences.");
+                System.out.println();
+            }
+        }
+        return;
+    }
+
+    public static void Lottery_System(int x_Coordinate, int y_Coordinate, int hp, Scanner scanner, Random random, int systemNumber) {
+        // Lottery System
+        if ((x_Coordinate == 3 && y_Coordinate == 3 || hp <= 0)) {
+            System.out.println("YOUR HP IS LESS THAN 0 : " + hp);
+            System.out.println("Lottery System , Guess a number in between 1 - 10 #:o");
+            int guess = scanner.nextInt();
+            scanner.nextLine();
+
+            systemNumber = random.nextInt(1, 11);
+            double temp1 = Math.abs((double) (guess - systemNumber) / systemNumber);
+
+            int hp_temp = hp < 0 ? Math.abs(hp) : hp;
+
+            if (guess == systemNumber) {
+                System.out.println("HP fully restored ;)");
+                hp = 100;
+            }
+
+            else if (temp1 >= 0.8) {
+                hp = (int) (hp + hp_temp * 1);
+                System.out.println("HP increased by 100% ;)" + "\nHP:" + hp);
+            }
+
+            else if (temp1 >= 0.5) {
+                hp = (int) (hp + hp_temp * 0.5);
+                System.out.println("HP increased by 50% ;)" + "\nHP:" + hp);
+            }
+
+            else {
+                hp = (int) (hp + hp_temp * 0.3);
+                System.out.println("HP increased by 30% ;)" + "\nHP:" + hp);
+            }
+
+            if (hp == 0) {
+                hp = 20;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the game!");
+        Description();
+
         // Player Info
         Scanner scanner = new Scanner(System.in);
 
@@ -120,121 +253,11 @@ public class Main {
             }
 
             counter++;
+            Movement(InGameMovement, x_Coordinate, y_Coordinate, StartingPoint, scanner);
 
-            if (x_Coordinate == 0 && y_Coordinate == 0) {
-                System.out.println("You are at the " + StartingPoint);
-            }
+            Villain(x_Coordinate, y_Coordinate, counter, PLAYERNAME, scanner, hp, score, villain_level, random);
 
-            // Movement System
-            System.out.println("Current Position: (" + x_Coordinate + ", " + y_Coordinate + ")");
-            System.out.println(
-                    "Where do you want to move? (Type 'up', 'down', 'left', or 'right')(max x and y are (5,5) and min x and y are (0,0))");
-            InGameMovement = scanner.nextLine();
-
-            switch (InGameMovement.toLowerCase()) {
-
-                case "down":
-                    if (y_Coordinate > 0) {
-                        y_Coordinate = y_Coordinate - 1;
-                    }
-                    break;
-                case "left":
-                    if (x_Coordinate > 0) {
-                        x_Coordinate = x_Coordinate - 1;
-                    }
-                    break;
-                case "right":
-                    if (x_Coordinate < 5) {
-                        x_Coordinate = x_Coordinate + 1;
-                    }
-                    break;
-                case "up":
-                    if (y_Coordinate < 5) {
-                        y_Coordinate = y_Coordinate + 1;
-                    }
-                    break;
-                default:
-                    System.out.println("Invalid movement, try again.");
-            }
-
-            // attack or defend
-            int x_Coordinate_v = random.nextInt(0, 5);
-            int y_Coordinate_v = random.nextInt(0, 5);
-
-            villain_level = random.nextInt(1, 11);
-
-            if (counter % 3 == 0) {
-                x_Coordinate_v = x_Coordinate;
-                y_Coordinate_v = y_Coordinate;
-            }
-            if ((x_Coordinate == x_Coordinate_v && y_Coordinate == y_Coordinate_v)) {
-                System.out.println("You are " + PLAYERNAME + "\n" + "A level " + villain_level
-                        + " villain has appear. BE PREPARED!!!");
-                GeminiChat.chat("You are a game villain. Choose a good name. You just encountered a player named "
-                        + PLAYERNAME + ". Introduce yourself dramatically in 30 words.");
-                System.out.println();
-                System.out.print(PLAYERNAME + " : ");
-                String reply = scanner.nextLine();
-
-                GeminiChat.chat(
-                        "You reply to the player reply in a way that after which is just action left. The reply is : "
-                                + reply);
-                System.out.println();
-
-                System.out.println("Do you want to attack or defend? (Type 'attack' or 'defend')");
-                String action = scanner.nextLine();
-
-                if (action.equalsIgnoreCase("attack")) {
-                    hp = hp - 10 * villain_level;
-                    score = score + 5 * villain_level;
-                    GeminiChat.chat(
-                            "Vilain went away but deal some damage. Appropriate dialogue for it as a closing scene in 2 sentences.");
-                    System.out.println();
-
-                } else if (action.equalsIgnoreCase("defend")) {
-                    hp = hp - 1 * villain_level;
-                    GeminiChat.chat(
-                            "Villain does not went away and deal some damage. Appropriate dialogue for it as a closing scene in 2 sentences.");
-                    System.out.println();
-                }
-            }
-
-            // Lottery System
-            if ((x_Coordinate == 3 && y_Coordinate == 3 || hp <= 0)) {
-                System.out.println("YOUR HP IS LESS THAN 0 : " + hp);
-                System.out.println("Lottery System , Guess a number in between 1 - 10 #:o");
-                int guess = scanner.nextInt();
-                scanner.nextLine();
-
-                systemNumber = random.nextInt(1, 11);
-                double temp1 = Math.abs((double) (guess - systemNumber) / systemNumber);
-
-                int hp_temp = hp < 0 ? Math.abs(hp) : hp;
-
-                if (guess == systemNumber) {
-                    System.out.println("HP fully restored ;)");
-                    hp = 100;
-                }
-
-                else if (temp1 >= 0.8) {
-                    hp = (int) (hp + hp_temp * 1);
-                    System.out.println("HP increased by 100% ;)" + "\nHP:" + hp);
-                }
-
-                else if (temp1 >= 0.5) {
-                    hp = (int) (hp + hp_temp * 0.5);
-                    System.out.println("HP increased by 50% ;)" + "\nHP:" + hp);
-                }
-
-                else {
-                    hp = (int) (hp + hp_temp * 0.3);
-                    System.out.println("HP increased by 30% ;)" + "\nHP:" + hp);
-                }
-
-                if (hp == 0) {
-                    hp = 20;
-                }
-            }
+            Lottery_System(x_Coordinate, y_Coordinate, hp, scanner, random, systemNumber);
         }
         scanner.close();
     }
